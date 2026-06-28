4e6edcb256a155ccc0a5e9b45338839f5d9ea203
Saranya Mohan <saranyamohantech@gmail.com>
2026-06-28 07:21:32 +0530

    Add SQL script to find highest order per customer
    This SQL script identifies the highest-value order placed by each customer using a Common Table Expression (CTE) and window functions.

---
+++ Advanced_SQL_Practice/Highest_Order_Per_Customer.sql
@@ -0,0 +1,35 @@
+WITH customer_orders AS
+(
+    SELECT
+        c.customer_name,
+        o.order_id,
+        o.amount,
+        MAX(o.amount) OVER
+        (
+            PARTITION BY c.customer_id
+        ) AS max_amount
+    FROM customers c
+    JOIN orders o
+        ON c.customer_id = o.customer_id
+)
+
+SELECT
+    customer_name,
+    order_id,
+    amount
+FROM customer_orders
+WHERE amount = max_amount
+ORDER BY customer_name;
+
+/*
+Business Requirement:
+
+Identify the highest-value order placed by each customer.
+
+Skills used:
+
+MAX()
+Window Functions
+CTE
+
+*/
