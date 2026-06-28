ca3a9e4884230069fd2de550d8c030ce676c05cb
Saranya Mohan <saranyamohantech@gmail.com>
2026-06-28 07:20:22 +0530

    Add SQL query for top 2 orders per customer
    Retrieve the top 2 highest-value orders for each customer using CTE and RANK() window function.

---
+++ Advanced_SQL_Practice/Top_2_Orders_Per_Customer.sql
@@ -0,0 +1,38 @@
+WITH ranked_orders AS
+(
+    SELECT
+        c.customer_name,
+        o.order_id,
+        o.amount,
+        RANK() OVER
+        (
+            PARTITION BY c.customer_id
+            ORDER BY o.amount DESC
+        ) AS rn
+    FROM customers c
+    JOIN orders o
+        ON c.customer_id = o.customer_id
+)
+
+SELECT
+    customer_name,
+    order_id,
+    amount
+FROM ranked_orders
+WHERE rn <= 2
+ORDER BY customer_name, amount DESC;
+
+
+/*
+
+Business Requirement:
+
+Retrieve the top 2 highest-value orders for each customer.
+
+Skills used:
+
+CTE
+RANK()
+Window Functions
+
+*/
